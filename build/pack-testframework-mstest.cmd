2ac520d337b67f39164c60c9f7a42a2f0b0356da
AdamMCraven <adam@ChannelAdam.com>
2015-02-24 21:40:40 +1000

    Updated mstest test framework to default to targetting .net 4.0

--- build/pack-testframework-mstest.cmd
+++ build/pack-testframework-mstest.cmd
@@ -1,3 +1,8 @@
-..\tools\nuget\nuget.exe pack ..\src\ChannelAdam.TestFramework.MSTest\ChannelAdam.TestFramework.MSTest.csproj -Build -Symbols -Properties Configuration=Release
+SET msbuild="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe"
+
+%msbuild% ..\src\ChannelAdam.TestFramework.MSTest\ChannelAdam.TestFramework.MSTest.csproj /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v4.0;OutDir=bin\Release\net40
+%msbuild% ..\src\ChannelAdam.TestFramework.MSTest\ChannelAdam.TestFramework.MSTest.csproj /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v4.5;OutDir=bin\Release\net45
+
+..\tools\nuget\nuget.exe pack ..\src\ChannelAdam.TestFramework.MSTest\ChannelAdam.TestFramework.MSTest.nuspec -Symbols
 
 pause
