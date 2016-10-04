bbf037cd399f5db7b6d1e6ea3a3ffb57ac4eb224
AdamMCraven <adam@ChannelAdam.com>
2016-10-04 22:53:36 +1000

    Updated AssemblyInfo in preparation for nuget packaging

--- src/ChannelAdam.TestFramework.BizTalk/Properties/AssemblyInfo.cs
+++ src/ChannelAdam.TestFramework.BizTalk/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ChannelAdam.TestFramework.BizTalk")]
@@ -10,14 +10,16 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ChannelAdam.TestFramework.BizTalk")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+[assembly: CLSCompliant(false)]     // TransformBase is not.
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2168cd38-757a-49e2-9e3f-d0e7a3931f42")]
@@ -25,12 +27,13 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]                  // Assembly strong name version
+[assembly: AssemblyFileVersion("1.0.0.0")]              // File version
+[assembly: AssemblyInformationalVersion("1.0.0")]       // NuGet package version / NuSpec $version$
